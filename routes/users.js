486480b7c2559e3e936307bb4e2e6433fa9d2385
Abdurrahman <abdomsdy0@gmail.com>
2024-12-27 12:28:25 +0300

    delete users

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/users');
-
-router.post('/', userController.create);
-router.get('/', userController.getAll);
-
-module.exports = router;
